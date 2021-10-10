4e8156ac4c61dbf56bb1bc694f939f4105897025
Macovei Tudor <tudor_macovei@iCloud.com>
2021-10-10 11:43:30 +0200

    Add communication and motion sensing
    The two NodeMCU boards are in a server-client relationship. The client automatically finds the server and sends status updates via HTML requests.

---
+++ simple_client/simple_client.ino
@@ -0,0 +1,62 @@
+#include <ESP8266WiFi.h>
+
+// Initialize sensor parameters
+bool isMotion = false;
+
+// Initialize network parameters
+const char *ssid = "ANY_SSID";
+const char *password = "ANY_PASSWORD";
+const char* host = "192.168.11.4"; // as specified in server.ino
+
+// Set up the client objet
+WiFiClient client;
+
+void setup()
+{
+  pinMode(D0, OUTPUT);  // LED On the board
+  pinMode(D1, INPUT);   // PIR motion sensor (2s approx delay)
+  Serial.begin(115200);
+
+  // Connect to the server
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("IP Address (AP): ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop()
+{
+  // Get a measurement from the sensor(s)
+  isMotion = digitalRead(D1);
+  if (isMotion) {
+    digitalWrite(D0, LOW);
+  }
+
+  // Connect to the server and send the data as a URL parameter
+  if (client.connect(host, 80))
+  {
+    String url = "/update?value=";
+    if (isMotion) {
+      url += "1.0";
+    } else {
+      url += "0.0";
+    }
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" +
+                 "Connection: keep-alive\r\n\r\n"); // minimum set of required URL headers
+    delay(20);
+    // Read all the lines of the response and print them to Serial
+    Serial.println("\nResponse: ");
+    while (client.available())
+    {
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+  }
+  delay(100);
+  digitalWrite(D0, HIGH);
+}
