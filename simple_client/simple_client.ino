3907791b229ba6b2f7ad2accc001b4a1520b1603
Macovei Tudor <tudor_macovei@iCloud.com>
2021-10-19 13:23:02 +0200

    Add all core functionality
    Issue: 6s delay between motion sensing and actuating the servos

--- simple_client/simple_client.ino
+++ simple_client/simple_client.ino
@@ -2,7 +2,12 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+#define MAX_TEMP 29.0
+// 29.0 is for testing purposes, should be switched in the final version
+
+// sensor variables
 bool isMotion = false;
+float temperature = 0.0;
 
 // Information for accessing the server
 const char *ssid = "ED_59";
@@ -13,17 +18,15 @@ const char *host = "192.168.11.4"; // IP address of server
 const int led_board = 16;     // D0 pin
 const int led_external_1 = 0; // D3 pin
 const int led_external_2 = 4; // D2 pin
-const int motion_sensor = 14;  // D5 pin (D4 pin is not working for some reason)
+const int motion_sensor = 14; // D5 pin (D4 pin is not working for some reason)
 const int temp_sensor = 5;    // D1 pin
+const int buzzer = 12;        // D6 pin
 
 unsigned long last_movement;
 
 WiFiClient client;
 OneWire oneWire(temp_sensor);
-
 DallasTemperature sensors(&oneWire);
-
-void contact_server();
 
 void server_connect() {
   WiFi.begin(ssid, password);
@@ -39,32 +42,56 @@ void server_connect() {
 
 void setup()
 {
+  Serial.begin(115200);
   pinMode(led_board, OUTPUT);  // LED On the board
   pinMode(motion_sensor, INPUT);   // PIR motion sensor
   pinMode(led_external_1, OUTPUT);
   pinMode(led_external_2, OUTPUT);
   pinMode(temp_sensor, INPUT);
-  
-//  pinMode(D6, INPUT);   // Temp Sensor input
-  Serial.begin(115200);
+  pinMode(buzzer, OUTPUT);
 
-  // Connect to the server
-//  server_connect();
-
+  server_connect();
   sensors.begin();
 }
 
 void loop()
 {
   // Get a measurement from the sensor(s)
+  sensors.requestTemperatures();                // Send the command to get temperatures  
   isMotion = digitalRead(motion_sensor);
+  temperature = sensors.getTempCByIndex(0);     // index 0 refers to the first IC on the wire (in our case we only have 1)
+  Serial.println("Temperature is: ");
+  Serial.println(temperature);   
+
+  // Connect to the server and send the data as a URL parameter
+  contact_server();
+  
+  debug_led();
+  control_led();  
+  control_buzzer();
+}
+
+void control_buzzer() {
+  if (temperature > MAX_TEMP) {
+     tone(buzzer, 2300); // Send 2.3KHz sound signal
+  } else {
+     noTone(buzzer);
+  }
+}
+
+void debug_led() {
   if (isMotion) {
     digitalWrite(led_board, LOW);
     last_movement = millis();
+  } else {
+    digitalWrite(led_board, HIGH);
   }
-  if (millis() - last_movement > 5000) {
+}
+
+void control_led() {
+  if (millis() - last_movement > 10000) {
     digitalWrite(led_external_1, LOW);
-    if (millis() - last_movement < 15000) {
+  if (millis() - last_movement < 15000) {
       digitalWrite(led_external_2, HIGH);  
     } else {
       digitalWrite(led_external_2, LOW);  
@@ -73,35 +100,35 @@ void loop()
     digitalWrite(led_external_1, HIGH);
     digitalWrite(led_external_2, LOW);  
   }
-  // Connect to the server and send the data as a URL parameter
-  
-  sensors.requestTemperatures();                // Send the command to get temperatures  
-  Serial.println("Temperature is: ");
-  Serial.println(sensors.getTempCByIndex(0));   // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  
-  contact_server();
-  delay(30);
-  digitalWrite(led_board, HIGH);
 }
 
 void contact_server() {
   if (client.connect(host, 80))
   {
     String url = "/update?value=";
-    if (!isMotion && millis() - last_movement > 5000) {
+    if (!isMotion && millis() - last_movement > 20000) {
+      // close servos / keep closed
       url += "0.0";
+      Serial.println("CLOSE");
     } else {
-      url += "1.0";
+      // open servos
+      if (temperature > MAX_TEMP) {
+        // close hot servo
+        url +="2.0";
+        Serial.println("CLOSE HOT");
+      } else {
+        // open servos / keep open
+        url += "1.0"; 
+        Serial.println("OPEN");
+      }
     }
     client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" +
                  "Connection: keep-alive\r\n\r\n"); // minimum set of required URL headers
     delay(20);
-    // Read all the lines of the response and print them to Serial
-    Serial.println("\nResponse: ");
+    // Read all the lines of the response
     while (client.available())
     {
       String line = client.readStringUntil('\r');
-      Serial.print(line);
     }
   }
 }
--- simple_webserver/simple_webserver.ino
+++ simple_webserver/simple_webserver.ino
@@ -2,9 +2,12 @@
 #include <ESP8266WebServer.h>
 #include <Servo.h>
 
+// NOTE: CLOCKWISE = OPEN, ANTI = CLOSE
+#define SPEED_CLOCKWISE 20
+#define SPEED_ANTICLOCK 160
+#define SPEED_STOP 90
+
 Servo cold, hot;
-const int servoCold = 14; // D5 pin
-//const int servoHot = 2;
 
 // Configuration parameters for server ("base station")
 char *ssid_ap = "ED_59";
@@ -16,14 +19,23 @@ IPAddress subnet(255, 255, 255, 0);
 ESP8266WebServer server;
 
 // Sensor data that's going to be recieved
-float motion_value = 0.0;
+float sensors_value = 0.0;
+
+// servo handling variables
+// !IMPORTANT: ball valles are closed at the start
+int should_hot_close = 1;
+int should_cold_close = 1;
+int is_hot_close = 1;
+int is_cold_close = 1;
 
 // define pinouts
-const int led_board = 16; // D0 pin
+const int led_board = 16; // D0 pin, debug purposes
+const int servoCold = 14; // D5 pin
+const int servoHot = 0;   // D3 pin
 
 void setup()
 {
-  pinMode(D0, OUTPUT); // LED On the board
+//  pinMode(led_board, OUTPUT);
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(ip, gateway, subnet);
   WiFi.softAP(ssid_ap, password_ap);
@@ -39,58 +51,94 @@ void setup()
   server.begin();
 
   cold.attach(servoCold);
-  //  hot.attach(servoHot);
+  hot.attach(servoHot);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // update servo control variables
   server.handleClient();
-  // turn the LED on the board on if motion is detected by client
-  if (motion_value > 0.5)
-  {
-    digitalWrite(D0, LOW);
-    //    hot_open();
-    cold_open();
+  // respond to the update
+  servo_control();
+  Serial.println(sensors_value);
+}
+
+void servo_control() {
+  if (should_hot_close && !is_hot_close) {
+    is_hot_close = 1;
+    hot_close();
   }
-  else
-  {
+  if (!should_hot_close && is_hot_close) {
+    is_hot_close = 0;
+    hot_open();
+  }
+  if (should_cold_close && !is_cold_close) {
+    is_cold_close = 1;
     cold_close();
-    digitalWrite(D0, HIGH);
+  }
+  if (!should_cold_close && is_cold_close) {
+    is_cold_close = 0;
+    cold_open();
   }
 }
 
-// these functions are causing a lot of issues, must fix since they take way too long
 void cold_open()
 {
-  for (int pos = 0; pos <= 180; pos += 1)
-  {
-    // goes from 0 degrees to 180 degrees in steps of 1 degree
-    cold.write(pos); // tell servo to turn 'pos' degrees (I think)
-    delay(15);       // waits 15ms for the servo to reach the position
-  }
+  cold.write(SPEED_CLOCKWISE);
+  delay(900);
+  cold.write(SPEED_STOP);
+  delay(100);
 }
 
 void cold_close()
 {
-  for (int pos = 0; pos <= 180; pos += 1)
-  {
-    // goes from 0 degrees to 180 degrees in steps of 1 degree
-    cold.write(-pos); // tell servo to turn 'pos' degrees (I think)
-    delay(15);        // waits 15ms for the servo to reach the position
-  }
+  cold.write(SPEED_ANTICLOCK);
+  delay(900);
+  cold.write(SPEED_STOP);
+  delay(100);
+}
+
+void hot_open()
+{
+  hot.write(SPEED_CLOCKWISE);
+  delay(900);
+  hot.write(SPEED_STOP);
+  delay(100);
+}
+
+void hot_close()
+{
+  hot.write(SPEED_ANTICLOCK);
+  delay(900);
+  hot.write(SPEED_STOP);
+  delay(100);
 }
 
 // debugging purposes
 void handleIndex()
 {
-  server.send(200, "text/plain", String(motion_value)); // refresh the page for getting the latest value (for debugging purposes)
+  server.send(200, "text/plain", String(sensors_value)); // refresh the page for getting the latest value (for debugging purposes)
 }
 
 void handleUpdate()
 {
   // The value will be passed as a URL argument
-  motion_value = server.arg("value").toFloat();
-  Serial.println(motion_value);
-  server.send(200, "text/plain", "Updated");
+  sensors_value = server.arg("value").toFloat();
+  server.send(200, "text/plain", "Updated!");
+  if (sensors_value > 0.5 && sensors_value < 1.5) {
+    Serial.println("I want to open both valves");
+    should_hot_close = 0;
+    should_cold_close = 0;
+  }
+  if (sensors_value > 1.5 && sensors_value < 2.5) {
+    Serial.println("I want to close the hot valve");
+    should_hot_close = 1;
+    should_cold_close = 0;
+  }
+  if (sensors_value > -0.5 && sensors_value < 0.5) {
+    Serial.println("I want to close both valves");
+    should_hot_close = 1;
+    should_cold_close = 1;
+  }
+  Serial.println(sensors_value);
 }
